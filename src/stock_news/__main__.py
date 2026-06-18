ea8d5ef5f879f85d3085fe997490a0ae3db6a878
saltchicken <john.eicher89@gmail.com>
2026-06-18 13:07:12 -0700

    Remove schedule

--- src/stock_news/__main__.py
+++ src/stock_news/__main__.py
@@ -1,28 +1,16 @@
-import time
 from datetime import datetime
 from loguru import logger
-import schedule
 
 from stock_news.config import setup_logging
 from stock_news.news import fetch_discovery_news
 
-def scheduled_job():
-    """Wrapper function to run all news gathering tasks."""
-    logger.debug(
-        f"--- RUNNING SCHEDULED FETCH: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} ---"
-    )
-    fetch_discovery_news()
-
 def main():
     """Entry point for command line execution."""
     setup_logging()
-    scheduled_job()
-    schedule.every(60).minutes.do(scheduled_job)
-
-    logger.debug("Stock Discovery Scheduler active. Waiting for next job...")
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+    logger.debug(
+        f"--- RUNNING FETCH: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} ---"
+    )
+    fetch_discovery_news()
 
 if __name__ == "__main__":
     main()
